a6a3cb83a97bbc397263bab60a83675df0a4609f
avitar64 <avishai@taragin.com>
2021-12-31 14:29:34 +0200

    player space movemnet and shooting working

--- player.go
+++ player.go
@@ -6,10 +6,21 @@ import (
 	pixgl "github.com/faiface/pixel/pixelgl"
 )
 
+const (
+	PlayerAccSpeed        = 0.93
+	// PlayerMaxSpeed        = 8
+	PlayerSlowSpeed       = 0.11
+	PlayerDragSpeed       = 3 // how much of player speed should we slow down
+	PlayerRotSpeed        = 0.07
+	PlayerCooldown        = time.Millisecond * 125
+	PlayerMissileSpeed    = 12
+	PlayerMissilePoolSize = 80
+)
+
 func NewPlayer(win *pixgl.Window) *Entity {
 	plr := NewEntity("sprites/player.png", win)
 
-	plr.expands = append(plr.expands, &PlayerControl{PlayerSpeed})
+	plr.expands = append(plr.expands, &PlayerControl{speed: PlayerAccSpeed})
 
 	var plrMissilePool []*Entity
 	for i := 0; i < PlayerMissilePoolSize; i++ {
@@ -25,25 +36,58 @@ func NewPlayer(win *pixgl.Window) *Entity {
 	plr.expands = append(plr.expands, &plrShooting)
 
 	plr.pos.X = win.Bounds().W() / 2
-	plr.pos.Y = win.Bounds().H() / 16
+	plr.pos.Y = win.Bounds().H() / 5
 
 	return plr
 }
 
 type PlayerControl struct {
-	speed float64
+	speed  float64
+	xv, yv float64
 }
 
 func (pc *PlayerControl) ExpandUpdate(p *Entity) error {
-	if p.KeyPressed(pixgl.KeyA) {
-		if !(p.pos.X-p.dim.width/2 < 0) {
-			p.pos.X -= pc.speed
-		}
+	if KeyPressed(p, pixgl.KeyA) {
+		p.rot += PlayerRotSpeed
 	}
-	if p.win.Pressed(pixgl.KeyD) {
-		if !(p.pos.X+p.dim.width/2 > WindowWidth) {
-			p.pos.X += pc.speed
-		}
+	if KeyPressed(p, pixgl.KeyD) {
+		p.rot -= PlayerRotSpeed
+	}
+
+	if KeyPressed(p, pixgl.KeyW) {
+		sxv, syv := ChangeXY(p.rot)
+		pc.xv += sxv * PlayerAccSpeed
+		pc.yv += syv * PlayerAccSpeed
+	}
+
+	// change x, y for player
+	if p.pos.X > WindowWidth+p.dim.width {
+		p.pos.X = 0
+	} else if p.pos.X < 0-p.dim.width {
+		p.pos.X = WindowWidth
+	}
+	p.pos.X += pc.xv
+
+	if p.pos.Y > WindowHeight+p.dim.height {
+		p.pos.Y = 0
+	} else if p.pos.Y < 0-p.dim.height {
+		p.pos.Y = WindowHeight
+	}
+	p.pos.Y += pc.yv
+
+	// slow down player
+	if pc.xv > 0 {
+		pc.xv -= PlayerSlowSpeed * pc.xv / PlayerDragSpeed
+	}
+	if pc.xv < 0 {
+		pc.xv += PlayerSlowSpeed * pc.xv / -PlayerDragSpeed
+	}
+
+	if pc.yv > 0 {
+		pc.yv -= PlayerSlowSpeed * pc.yv / PlayerDragSpeed
+	}
+	if pc.yv < 0 {
+		pc.yv += PlayerSlowSpeed * pc.yv / -PlayerDragSpeed
 	}
 
 	return nil
@@ -56,34 +100,45 @@ type PlayerShooting struct {
 	lastShot     time.Time
 }
 
+func (ps *PlayerShooting) MoveMissileOffPlayer(missile, plr *Entity) {
+	xv, yv := ChangeXY(missile.rot)
+	missile.pos.X += xv * plr.dim.width / 2
+	missile.pos.Y += yv * plr.dim.height / 2
+}
+
 func (ps *PlayerShooting) ExpandUpdate(p *Entity) error {
 	for _, missile := range ps.pool {
 		if missile.active {
-			missile.pos.Y += ps.missileSpeed
-			if missile.pos.Y > WindowHeight {
+			// missile.pos.Y += ps.missileSpeed
+			xv, yv := ChangeXY(missile.rot)
+			missile.pos.X += xv * ps.missileSpeed
+			missile.pos.Y += yv * ps.missileSpeed
+			if missile.pos.Y > WindowHeight || missile.pos.Y < 0 || missile.pos.X > WindowWidth || missile.pos.X < 0{
 				missile.active = false
-			} else {
-				missile.Draw()
-			}
-		}
-	}
-	if p.KeyPressed(pixgl.KeySpace) || p.KeyPressed(pixgl.KeyS) {
-		if time.Since(ps.lastShot) >= ps.cooldown {
-			var missile *Entity
-
-			// find un active missile and activate it after putting it in the right place
-			for i, missileInPool := range ps.pool {
-				if !missileInPool.active {
-					missile = ps.pool[i]
-					break
+				} else {
+					missile.Draw()
 				}
 			}
-
-			missile.active = true
-			missile.pos = p.pos
-
-			ps.lastShot = time.Now()
 		}
+		if KeyPressed(p, pixgl.KeySpace) || KeyPressed(p, pixgl.KeyS) {
+			if time.Since(ps.lastShot) >= ps.cooldown {
+				var missile *Entity
+		
+				// find un active missile and activate it after putting it in the right place
+				for i, missileInPool := range ps.pool {
+					if !missileInPool.active {
+						missile = ps.pool[i]
+						break
+					}
+				}
+		
+				missile.active = true
+				missile.rot = p.rot
+				missile.pos = p.pos
+				ps.MoveMissileOffPlayer(missile, p)
+		
+				ps.lastShot = time.Now()
+			}
+		}
+		return nil
 	}
-	return nil
-}
--- utils.go
+++ utils.go
@@ -1,36 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"os"
 	"time"
+	"math"
 
 	pix "github.com/faiface/pixel"
-	pgl "github.com/faiface/pixel/pixelgl"
+	pixgl "github.com/faiface/pixel/pixelgl"
 )
 
 const (
-	FPS = 50  // ! not exact !!
+	FPS                   = 50 // ! not exact !!
 	MillersecondsPerFrame = 1000 / FPS
 
-	WindowWidth = 624
-	WindowHeight = 912
-
-	PlayerSpeed = 7
-	PlayerCooldown = time.Millisecond * 125
-	PlayerMissileSpeed = 12
-	PlayerMissilePoolSize = 80
+	WindowWidth  = 1024
+	WindowHeight = 768
 )
 
 func Initialize(windowTitle string) *Game { // (*pgl.WindowConfig, *pgl.Window, *FrameLimiter)
-	wcfg := pgl.WindowConfig{
+	wcfg := pixgl.WindowConfig{
 		Title:  windowTitle,
 		Bounds: pix.R(0, 0, WindowWidth, WindowHeight),
 	}
-	win, err := pgl.NewWindow(wcfg)
+	win, err := pixgl.NewWindow(wcfg)
 	if err != nil {
 		panic(err)
 	}
+	win.SetSmooth(true)
 
 	framer := NewFrameLimiter()
 
@@ -50,4 +48,117 @@ func LoadPicture(path string) (pix.Picture, error) {
 		return nil, err
 	}
 	return pix.PictureDataFromImage(img), nil
+}
+
+func ChangeXY(a float64) (float64, float64) {
+	// return math.Cos(a), -1 * math.Sin(a)
+	return -1 * math.Sin(a), math.Cos(a)
+}
+
+func KeyPressed(e *Entity, key pixgl.Button) bool {
+	return e.win.Pressed(key)
+}
+
+func NewEntity(pictureFile string, win *pixgl.Window) *Entity {
+	e := Entity{}
+
+	img, err := LoadPicture(pictureFile)
+	if err != nil {
+		panic(fmt.Errorf("can't load picture %v, to create a new Entity: %v", pictureFile, err))
+	}
+	spr := pix.NewSprite(img, img.Bounds())
+	rect := img.Bounds()
+	width := rect.Max.X - rect.Min.X
+	height := rect.Max.Y - rect.Min.Y
+
+	e.active = true
+	e.rot = 0
+	e.pos = pix.Vec{X: 0, Y: 0}
+	e.dim = Dimension{width: width, height: height}
+	e.img = img
+	e.spr = spr
+	e.win = win
+
+	return &e
+}
+
+// // spacer
+
+type Entity struct {
+	active  bool
+	rot     float64       // rotation
+	pos     pix.Vec       // postition
+	dim     Dimension     // dimensions
+	img     pix.Picture   // image
+	spr     *pix.Sprite   // sprite
+	win     *pixgl.Window // window
+	expands []Expantion   // expansions
+}
+
+type Dimension struct {
+	width, height float64
+}
+
+type Expantion interface {
+	ExpandUpdate(*Entity) error
+}
+
+func (e *Entity) Draw() {
+	if e.active {
+		mat := pix.IM
+		mat = mat.Moved(e.pos)
+		mat = mat.Rotated(e.pos, e.rot)
+
+		e.spr.Draw(e.win, mat)
+	}
+}
+
+func (e *Entity) Update() error {
+	for _, expansion := range e.expands {
+		err := expansion.ExpandUpdate(e)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// // spacer
+
+func NewFrameLimiter() *FrameLimiter {
+	return &FrameLimiter{
+		millisPerFrame: MillersecondsPerFrame,
+		last:           time.Now(),
+	}
+}
+
+type FrameLimiter struct {
+	millisPerFrame int64
+	last           time.Time
+	frames         uint
+	second         <-chan time.Time
+}
+
+func (f *FrameLimiter) ShouldDoNextFrame() bool {
+	dt := time.Since(f.last).Milliseconds()
+	if dt > f.millisPerFrame {
+		f.last = time.Now()
+		return true
+	}
+	return false
+}
+
+func (f *FrameLimiter) InitFrameCounter() {
+	f.frames = 0
+	f.second = time.Tick(time.Second)
+}
+
+func (f *FrameLimiter) SetTitleWithFPS(win *pixgl.Window, wcfg *pixgl.WindowConfig) {
+	f.frames++
+	select {
+	case <-f.second:
+		win.SetTitle(fmt.Sprintf("%s | FPS: %d", wcfg.Title, f.frames))
+		f.frames = 0
+	default:
+	}
 }
